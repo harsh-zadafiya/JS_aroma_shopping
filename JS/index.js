ddf4a8f338e8c3ab4b0a52e6abac78caa910b31a
Ankit Maniya <66209504+ankit-maniya@users.noreply.github.com>
2023-04-21 13:01:23 -0400

    carasoul added in home page

---
+++ JS/index.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(".product-wrapper").slick({
+      dots: true,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      adaptiveHeight: true,
+    });
+  });
